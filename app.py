aeb8a5fc74741ad3d6fcd1134bff684c663507a2
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2013-09-17 16:23:54 -0700

    macro extractions, monthly view, styling

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import datetime
 import database
-from flask import Flask, render_template #, url_for
+from dateutil.relativedelta import relativedelta
+import calendar
+from flask import Flask, render_template, redirect, url_for
 
-ARCHIVE_LISTING_SHOTS = 3*2 + 6*3
+
+ARCHIVE_LISTING_SHOTS = 3*2 + 6*4
 HOME_TODAY_SHOTS = 6
+CALENDAR_START = datetime.date(2013, 05, 30)
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
@@ -13,16 +18,43 @@ def home():
     return render_template('pages/home.html', today_popular=today_popular,
                            today=today_utc)
 
+
 @app.route('/archive/<int:year>/<int:month>/<int:day>/')
 def archive_day(year, month, day):
     date = datetime.date(year, month, day)
     shots = database.popular_shots_of_day(date, ARCHIVE_LISTING_SHOTS)
     return render_template('pages/archive_day.html', shots=shots,
-                           day=date)
+                            day=date)
+
 
 @app.route('/archive/<int:year>/<int:month>/')
 def archive_month(year, month):
-    return "ok"
+    calendar_start_month = datetime.date(CALENDAR_START.year,
+                                         CALENDAR_START.month, 1)
     
+    first_month_redir = redirect(url_for('archive_month',
+                                        year=CALENDAR_START.year,
+                                        month=CALENDAR_START.month));
+    if year == 0 or month == 0:
+        return first_month_redir
+
+    date = datetime.date(year, month, 1)
+    if date < calendar_start_month:
+        return first_month_redir
+
+    cal = calendar.Calendar()
+    days = list(cal.itermonthdays2(date.year, date.month))
+
+    today = datetime.datetime.utcnow().date()
+    prev = date - relativedelta(months=1)
+    next = date + relativedelta(months=1)
+    prev_disabled = prev < calendar_start_month
+    next_disabled = next > today
+
+    return render_template('pages/archive_month.html', date=date, days=days,
+                            prev=prev, prev_disabled=prev_disabled,
+                            next=next, next_disabled=next_disabled,
+                            today=today)
+
 if __name__ == '__main__':
     app.run(debug=True)
