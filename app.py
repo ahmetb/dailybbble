eefb4e0d277781e0e7ab17325599807f32d3a8dc
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2013-09-17 17:05:23 -0700

    add host param to app

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def archive_month(year, month):
 if __name__ == '__main__':
     port = int(os.environ['PORT']) if 'PORT' in os.environ else None
     debug = True if not port else False
-    app.run(port=port, debug=debug)
+    app.run(host='0.0.0.0', port=port, debug=debug)
